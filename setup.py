828db1fdacfaeba6eec9ea22652f53e7f032a065
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-10-26 15:48:35 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'chatbot Ananlysis',
+    version= '0.0.0',
+    author= 'siddhanath tiwari',
+    author_email= 'pratimatiwari8299@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
